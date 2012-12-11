56497f38798df19e9b1e68b0a3f7af8cdb975196
Maarten de Jonge <mdejonge1990@gmail.com>
2012-12-11 12:55:19 +0100

    add awesome's rc.lua

--- symlink_config.sh
+++ symlink_config.sh
@@ -8,3 +8,10 @@ ln -s $(pwd)/.Xresources ~
 ln -s $(pwd)/.tmux.conf ~
 
 ln -s $(pwd)/.fonts ~
+
+# check if there's already a .config/awesome folder, and if not create it
+if [ ! -d "~/.config/awesome" ]; then
+    mkdir -p ~/.config/awesome
+fi
+
+ln -s $(pwd)/rc.lua ~/.config/awesome/
