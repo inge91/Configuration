5a9a59ddf86ba5c2850b6410ae5fc1e5cfc4494d
Maarten de Jonge <mdejonge1990@gmail.com>
2013-04-04 13:31:25 +0200

    fix some dependencies in emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,7 +10,7 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(auctex evil magit color-theme-sanityinc-tomorrow scala-mode2 prolog)
+(defvar my-packages '(auctex evil magit color-theme-sanityinc-tomorrow scala-mode2 prolog tree-mode yasnippet)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
