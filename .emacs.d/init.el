188dba59ca0c52be4e48641aeccf1a1fea9b3306
Maarten de Jonge <mdejonge1990@gmail.com>
2013-04-20 00:11:12 +0200

    add more niceties to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,9 +11,9 @@
 
 ;; Add in your own as you wish:
 (defvar my-packages '(auctex evil magit ir-black-theme scala-mode2 prolog
-                             tree-mode yasnippet scala-mode2 wrap-region cdlatex
+                             tree-mode yasnippet scala-mode2 wrap-region 
                              color-theme-sanityinc-tomorrow auto-complete
-                             powerline)
+                             powerline autopair dtrt-indent)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
@@ -22,13 +22,14 @@
 
 ;;;;;; local packages ;;;;;;
 (add-to-list 'load-path "~/.emacs.d/local")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/local/themes")
 
 ;; enable evil mode
 (evil-mode 1)
 
 ;; gotta look sexy
-(set-frame-font "Monaco-9")
-(load-theme 'sanityinc-tomorrow-night t)
+(set-frame-font "Dejavu Sans Mono-9")
+(load-theme 'railscasts t)
 
 (setq make-backup-files nil)
 (setq auto-save-default nil)
@@ -47,15 +48,13 @@
 
 ;; linebreak
 (set-fill-column 80)
-(auto-fill-mode t)
+(setq auto-fill-mode t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
- '(org-hide-emphasis-markers t)
  '(preview-gs-options (quote ("-q" "-dNOPAUSE" "-DNOPLATFONTS" "-dPrinted" "-dTextAlphaBits=4" "-dGraphicsAlphaBits=4"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -68,14 +67,22 @@
 (require 'yasnippet)
 (require 'auto-complete-config)
 (require 'powerline)
+(require 'ido)
+(require 'autopair)
+(require 'dtrt-indent)
 
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
-(add-hook 'LaTeX-mode-hook 'turn-on-cdlatex)
 
 (ac-config-default)
-(yas/global-mode 1)
+(yas/global-mode t)
+(ido-mode t)
+(autopair-global-mode)
+(dtrt-indent-mode t)
 
 (add-to-list 'load-path "~/.emacs.d/local/ensime/elisp/")
 (require 'ensime)
 
 (powerline-default-theme)
+
+(setq ido-enable-flex-matching t)
+(setq org-hide-emphasis-markers t)
