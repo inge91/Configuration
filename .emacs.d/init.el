6d40d834e8ad4188fdd40382eb534a0e9d233443
Maarten de Jonge <mdejonge1990@gmail.com>
2013-04-27 12:46:52 +0200

    add emacs org agenda, change OB theme (no deco)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -59,6 +59,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Dropbox/Org/thesis.org")))
  '(preview-gs-options (quote ("-q" "-dNOPAUSE" "-DNOPLATFONTS" "-dPrinted" "-dTextAlphaBits=4" "-dGraphicsAlphaBits=4"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
