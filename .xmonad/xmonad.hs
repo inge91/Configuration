f0c9a67d511064557c0ec4ac0b97aec8bfe31aaa
Maarten de Jonge <mdejonge1990@gmail.com>
2013-11-11 03:15:33 +0100

    update xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,12 +6,14 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout.ResizableTile
+import XMonad.Layout.Gaps
 
 import XMonad.Hooks.DynamicLog  
 import XMonad.Hooks.ManageDocks  
 
-myLayout = smartSpacing 7 (tiled ||| threecol) ||| noBorders Full
+myLayout = (mygaps $ spacing 4 (tiled ||| threecol)) ||| noBorders Full
     where
+        mygaps = gaps [(U, 4), (D, 4), (L, 4), (R, 4)]
         tiled = ResizableTall 1 (3/100) (1/2) []
         threecol = ThreeColMid 1 (3/100) (1/2)
 
@@ -23,13 +25,13 @@ myPP = xmobarPP
 myToggleStruts XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 main = xmonad =<< statusBar myBar myPP myToggleStruts defaultConfig
-    { terminal           = "xcwd | xargs urxvt -cd"
+    { terminal           = "urxvt"
     , modMask            = mod4Mask
     , borderWidth        = 2
     , layoutHook         = myLayout
     , manageHook         = manageDocks <+> manageHook defaultConfig
-    , normalBorderColor  = "#EEEEEE"
-    , focusedBorderColor = "#FF0000"
+    , normalBorderColor  = "#151515"
+    , focusedBorderColor = "#E1AA5D"
     , keys               = myKeys <+> keys defaultConfig
     }
 
